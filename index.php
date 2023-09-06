106f21dae6e9cad329548a8257a1861ab5ddbe23
UNESUM <68912218+jonathan-conforme@users.noreply.github.com>
2023-09-06 00:01:41 -0500

    h7

--- index.php
+++ index.php
@@ -292,7 +292,7 @@
         </nav>
       </div>
     </div>
-
+    <?php include ("includes/carrusel.php"); ?>  
     <main class="container">
       <div class="p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary">
         <div class="col-lg-6 px-0">
